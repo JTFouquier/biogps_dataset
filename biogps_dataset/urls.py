8439f94af0a50b241699e0fbabf9f41dc8c1f6cf
jin xuefeng <172174484@qq.com>
2014-07-08 21:48:29 +0800

    static files for dataset page

--- biogps_dataset/urls.py
+++ biogps_dataset/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-
+from biogps_dataset import settings
 from django.contrib import admin
 admin.autodiscover()
 
@@ -11,3 +11,4 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^grappelli/', include('grappelli.urls')), # grappelli URLS
 )
+urlpatterns += static.static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
