62cff118689d29cee768b69cc257bb6388c655e4
jin xuefeng <172174484@qq.com>
2014-07-30 00:51:44 +0800

    code refine

--- dataset/urls.py
+++ dataset/urls.py
@@ -1,10 +1,9 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns,  url
 import views
 
 urlpatterns = patterns('',
     url(r'^info/$', views.dataset_info, name='dataset info'),
     url(r'^data/$', views.dataset_data, name='dataset data'),
     url(r'^chart/$', views.dataset_chart, name='dataset chart'),
-    url(r'^cvs/$', views.get_cvs, name='get cvs'),
+    url(r'^csv/$', views.get_csv, name='dataset csv'),
 )
-
