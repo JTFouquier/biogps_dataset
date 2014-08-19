1a04eda8fbd1dbae72349956d53ee4ca61185f1a
jin xuefeng <172174484@qq.com>
2014-08-19 17:46:47 +0800

    1)add document after delete to prevent duplication 2)add reset button on page

--- dataset/management/commands/es_index.py
+++ dataset/management/commands/es_index.py
@@ -8,6 +8,7 @@ class Command(BaseCommand):
         qs = models.BiogpsDataset.objects.all()
         es = Elasticsearch()
         for item in qs:
+            es.delete(index="blogs", doc_type="biogps", id=item.id)
             es.index(index="blogs", doc_type="biogps",  \
                     body=item.es_index_serialize(), id=item.id)
         print "%d datasets added to ES." % qs.count()
