05e7c2035823551d0fd516c25ea13d5029f5e2d5
jin xuefeng <172174484@qq.com>
2014-08-19 23:14:38 +0800

    continue to last commitq

--- dataset/urls.py
+++ dataset/urls.py
@@ -2,11 +2,13 @@ from django.conf.urls import patterns,  url
 import views
 
 urlpatterns = patterns('',
-    url(r'^info/$', views.dataset_info, name='dataset info'),
-    url(r'^data/$', views.dataset_data, name='dataset data'),
-    url(r'^(?P<_id>\d+)/chart/(?P<reporter>.+)/$', views.dataset_chart,\
-        name='dataset chart'),
+    url(r'^meta/(?P<ds_id>\d+)/$', views.dataset_info, name='dataset meta'),
+    url(r'^data/(?P<ds_id>\d+)/gene/(?P<gene_id>\d+)/$', \
+        views.dataset_data, name='dataset data'),
+    url(r'^chart/(?P<ds_id>\d+)/reporter/(?P<reporter_id>.+)/$',\
+         views.dataset_chart, name='dataset chart'),
     #url(r'^chart/$', views.dataset_chart, name='dataset chart'),
-    url(r'^csv/$', views.dataset_csv, name='dataset csv'),
+    url(r'^csv/(?P<ds_id>\d+)/gene/(?P<gene_id>.+)/$', \
+        views.dataset_csv, name='dataset csv'),
     url(r'^search/$', views.dataset_search, name='dataset search')
 )
