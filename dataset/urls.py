3742117823400fe7460d3f1c9533dd8f5d3f49f3
jin xuefeng <172174484@qq.com>
2014-08-20 00:04:31 +0800

    support hard code default dataset

--- dataset/urls.py
+++ dataset/urls.py
@@ -2,13 +2,13 @@ from django.conf.urls import patterns,  url
 import views
 
 urlpatterns = patterns('',
-    url(r'^meta/(?P<ds_id>\d+)/$', views.dataset_info, name='dataset meta'),
-    url(r'^data/(?P<ds_id>\d+)/gene/(?P<gene_id>\d+)/$', \
+    url(r'^meta/(?P<ds_id>.+)/$', views.dataset_info, name='dataset meta'),
+    url(r'^data/(?P<ds_id>.+)/gene/(?P<gene_id>\d+)/$', \
         views.dataset_data, name='dataset data'),
-    url(r'^chart/(?P<ds_id>\d+)/reporter/(?P<reporter_id>.+)/$',\
+    url(r'^chart/(?P<ds_id>.+)/reporter/(?P<reporter_id>.+)/$',\
          views.dataset_chart, name='dataset chart'),
     #url(r'^chart/$', views.dataset_chart, name='dataset chart'),
-    url(r'^csv/(?P<ds_id>\d+)/gene/(?P<gene_id>.+)/$', \
+    url(r'^csv/(?P<ds_id>.+)/gene/(?P<gene_id>.+)/$', \
         views.dataset_csv, name='dataset csv'),
     url(r'^search/$', views.dataset_search, name='dataset search')
 )
