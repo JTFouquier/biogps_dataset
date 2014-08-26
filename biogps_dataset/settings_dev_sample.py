8f92f3705e0ec2acefeab8cbda5cb56cce827830
jin xuefeng <172174484@qq.com>
2014-08-26 16:36:01 +0800

    add gene settings to sample settings

--- biogps_dataset/settings_dev_sample.py
+++ biogps_dataset/settings_dev_sample.py
@@ -10,10 +10,36 @@ DATABASES = {
     }
 }
 
+#if cache http responses when fetch data
+#used in ds load
 CACHE_HTTP_DATA = False
 
 #dataset mining takes long time, debug mode may leak memory
 #set False then
 DEBUG = True
 
+#default gene id
 DEFAULT_GENE_ID = 1017
+
+#gene taxid, dataset default id mapping
+DEFAULT_DATASET_MAPPING = {
+    # human
+    9606: 10,
+    # mouse
+    10090: 10,
+    # rat
+    10116: 10,
+    # fruit fly
+    7227: 10,
+    # nematode
+    6239: 10,
+    # zebrafish
+    7955: 10,
+    # thale-cress
+    3702: 10,
+    # frog
+    8364: 10,
+    # pig
+    9823: 10,
+}
+
