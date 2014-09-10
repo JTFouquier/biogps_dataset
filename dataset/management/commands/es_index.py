ca34e5348c6325a0d57213f0c763455fe7fdaf61
jin xuefeng <172174484.com@localhost>
2014-09-10 09:48:08 +0800

    error urllib

--- dataset/management/commands/es_index.py
+++ dataset/management/commands/es_index.py
@@ -1,29 +1,31 @@
 from dataset import models
 from elasticsearch import  Elasticsearch
 from django.core.management.base import BaseCommand
-import urllib2
+import urllib
 import json
-
+from django.conf import settings
+import urllib.request
+import urllib.response
+import urllib.error
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
 
-        request = urllib2.Request(r"http://localhost:9200/biogps/")
-        request.get_method = lambda: 'DELETE'
+        request = urllib.request.Request(settings.ES_BIOGPS,\
+                                          method='DELETE')
         try:
-            urllib2.urlopen(request)
+            urllib.urlopen(request)
             print "delete biogps index success"
-        except urllib2.HTTPError:
+        except urllib.error.HTTPError:
             print "no existing biogps index, continue."
         except Exception, e:
             print e
             print 'clear exist biogps index failed, leave.'
             return
 
-        request = urllib2.Request(\
-                r"http://localhost:9200/biogps/")
-        request.get_method = lambda: 'PUT'
-        urllib2.urlopen(request)
+        request = urllib.request.Request(\
+                r"http://localhost:9200/biogps/", method='PUT')
+        urllib.urlopen(request)
         print 'create index for biogps success'
 
         data = json.dumps({
@@ -32,11 +34,9 @@ class Command(BaseCommand):
                 "platform": {"type": "string", "store": True},
                 "reporters": {"type": "string", "store": True},
                 "id": {"type": "string", "store": True}}}})
-        request = urllib2.Request(\
-                r"http://localhost:9200/biogps/platform/_mapping",\
-                data=data)
-        request.get_method = lambda: 'PUT'
-        urllib2.urlopen(request)
+        request = urllib.request.Request(settings.ES_URL, \
+                                         data=data, method='PUT')
+        urllib.urlopen(request)
         print "create platform mapping success"
 
         data = json.dumps({
@@ -46,10 +46,8 @@ class Command(BaseCommand):
                 "name": {"type": "string", "store": True},
                  "summary": {"type": "string", "store": True},
                   "id": {"type": "string", "store": True}}}})
-        url = "http://localhost:9200/biogps/dataset/_mapping"
-        request = urllib2.Request(url, data=data)
-        request.get_method = lambda: 'PUT'
-        urllib2.urlopen(request)
+        request = urllib.request.Request(settings.ES_URL, data=data, method='PUT')
+        urllib.urlopen(request)
         print "create dataset mapping success"
 
         plt_body = {"platform": "", "reporters": "", "id": ""}
@@ -60,20 +58,17 @@ class Command(BaseCommand):
             plt_body["reporters"] = item.reporters
             plt_body["platform"] = item.platform
             data = json.dumps(plt_body)
-            plt_url = r"http://localhost:9200/biogps/platform/" + str(item.id)
-            request = urllib2.Request(plt_url, data=data)
-            request.get_method = lambda: 'POST'
-            urllib2.urlopen(request)
+            plt_url = settings.ES_PLAT + str(item.id)
+            request = urllib.request.Request(plt_url, data=data, method='POST')
+            urllib.urlopen(request)
             plt_count = plt_count + 1
             for bio_item in item.dataset_platform.all():
                 bio_body = bio_item.es_index_serialize()
                 date = json.dumps(bio_body)
-                bio_url = r"http://localhost:9200/biogps/dataset/" + \
+                bio_url = settings.ES_DATASET + \
                 str(bio_item.id) + "?parent=" + plt_body["id"]
-                request = urllib2.Request(bio_url, data=date)
-                request.get_method = lambda: 'PUT'
-                request.add_header('User-Agent', 'fake-client')
-                urllib2.urlopen(request)
+                request = urllib.request.Request(bio_url, data=date, method='PUT')
+                urllib.urlopen(request)
                 bio_count = bio_count + 1
 
         print "add %d platform , add %d dataset" % (plt_count, bio_count)
