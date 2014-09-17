1ea00d4b850a77665c9cd4127df66cd5b4a22fae
Xu <Xu@localhost>
2014-09-17 17:49:28 +0800

    ds_importa

--- dataset/management/commands/ds_import.py
+++ dataset/management/commands/ds_import.py
@@ -5,7 +5,7 @@ from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):             
     def handle(self, *args, **options): 
-        ds_plat = models.dataset_biogpsdatasetplatform.objects.using("default_ds").all()
+        ds_plat = models.BiogpsDatasetPlatform.objects.using("default_ds").all()
         for  plt_item in ds_plat:
             ds_sets = plt_item.dataset_platform
             plt_item.using("default_dataset").save()
