d8b43a0a11dccdeca16875933c4962057998cc47
Jin Xuefeng <Jin Xuefeng@localhost>
2014-11-03 23:34:00 +0800

    lint warning remove

--- dataset/management/commands/es_index.py
+++ dataset/management/commands/es_index.py
@@ -1,6 +1,5 @@
-#-*-coding: utf-8 -*-
+# -*-coding: utf-8 -*-
 from dataset import models
-from elasticsearch import  Elasticsearch
 from django.core.management.base import BaseCommand
 import json
 from django.conf import settings
@@ -25,21 +24,21 @@ class Command(BaseCommand):
         print 'create index for biogps success'
 
         data = json.dumps({
-        "platform": {
-            "properties": {
-                "platform": {"type": "string", "store": True},
-                "reporters": {"type": "string", "store": True}}}})
+            "platform": {
+                "properties": {
+                    "platform": {"type": "string", "store": True},
+                    "reporters": {"type": "string", "store": True}}}})
         requests.put(settings.ES_URLS['PF_C'], data=data)
         print "create platform mapping success"
 
         data = json.dumps({
-        "dataset": {
-        "_parent": {"type": "platform"},
-            "properties": {
-                "default":{"type":"integer","store":True},
-                "name": {"type": "string", "store": True},
-                 "summary": {"type": "string", "store": True},
-                  "geo_gse_id": {"type": "string", "store": True}}}})
+            "dataset": {
+                "_parent": {"type": "platform"},
+                "properties": {
+                    "default": {"type": "integer", "store": True},
+                    "name": {"type": "string", "store": True},
+                    "summary": {"type": "string", "store": True},
+                    "geo_gse_id": {"type": "string", "store": True}}}})
         requests.put(settings.ES_URLS['DS_C'], data=data)
         print "create dataset mapping success"
 
@@ -53,30 +52,31 @@ class Command(BaseCommand):
             plt_url = settings.ES_URLS['PF'] + str(plt_count)
             requests.post(plt_url, data=data)
             plt_count = plt_count + 1
-            #temp_data中的default字段表面了该document来自那个数据库，整数1表明来自默认数据库
+            # temp_data中的default字段表面了该document来自那个数据库，整数1表明来自默认数据库
             for ds in item.dataset_platform.all():
                 temp_data = ds.es_index_serialize()
-                temp_data["default"] =  0
+                temp_data["default"] = 0
                 data = json.dumps(temp_data)
                 url = settings.ES_URLS['DS'] + \
                     str(bio_count) + "?parent=" + str(plt_count - 1)
                 requests.put(url, data=data)
                 bio_count = bio_count + 1
 
-        print "from 'default' database,added %d platform , added %d dataset" % (plt_count, bio_count)
+        print "from 'default' database,added %d platform , added %d dataset" %\
+            (plt_count, bio_count)
 
         dataset = models.BiogpsDataset.objects.using("default_ds").\
-        filter(id__in=settings.DEFAULT_DS_ID)
+            filter(id__in=settings.DEFAULT_DS_ID)
         plt_ds, bio_ds = plt_count, bio_count
 
         plt_dic = {}
-        for ds_item in dataset:
-            plt_temp = ds_item.platform
+        for ds in dataset:
+            plt_temp = ds.platform
             plt_body["reporters"] = plt_temp.reporters
             plt_body["platform"] = plt_temp.platform
-            #plt_id用于保存插入dataset时对应的plt的id(esindex)
+            # plt_id用于保存插入dataset时对应的plt的id(esindex)
             plt_id = plt_count
-            if plt_dic.get(str(plt_temp.id), None) == None:
+            if plt_dic.get(str(plt_temp.id), None) is None:
                 data = json.dumps(plt_body)
                 plt_url = settings.ES_URLS['PF'] + str(plt_count)
                 requests.post(plt_url, data=data)
@@ -85,13 +85,13 @@ class Command(BaseCommand):
             else:
                 plt_id = plt_dic.get(str(plt_temp.id))
 
-            #temp_data中的default字段表面了该document来自那个数据库，整数0表明来自数据库default_ds
+            # temp_data中的default字段表面了该document来自那个数据库，整数1表明来自数据库default_ds
             temp_data = ds.es_index_serialize()
-            temp_data["default"] =  1
+            temp_data["default"] = 1
             data = json.dumps(temp_data)
             url = settings.ES_URLS['DS'] + \
-                    str(bio_count) + "?parent=" + str(plt_id)
+                str(bio_count) + "?parent=" + str(plt_id)
             requests.put(url, data=data)
             bio_count = bio_count + 1
         print "from 'default_ds' database, added %d platform, added %d dataset"\
-             % (plt_count - plt_ds, bio_count - bio_ds)
+            % (plt_count - plt_ds, bio_count - bio_ds)
