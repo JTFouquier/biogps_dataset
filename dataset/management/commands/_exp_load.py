2477d52fad60fb3d2d94c99ac0e1878332d0b7eb
Jin Xuefeng <Jin Xuefeng@localhost>
2014-11-03 14:41:24 +0800

    adjust search reset button position

--- dataset/management/commands/_exp_load.py
+++ dataset/management/commands/_exp_load.py
@@ -42,7 +42,7 @@ class Platform(ResourceRequest):
         split = split[start:]
         self.reporters = []
         for s in split:
-            self.reporters.append(s.strip().split('\t')[0])
+            self.reporters.append(s.strip().split('\s')[0])
         self.reporters.sort()
 
     def load_exps(self):
