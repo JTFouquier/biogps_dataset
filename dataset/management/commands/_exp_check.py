dba55206564d7fca0e6a04d16ae1901a49b98f86
Jin Xuefeng <Jin Xuefeng@localhost>
2014-11-10 15:24:38 +0800

    fix a bug in ds checkor(caused by aptana auto format) and remove lint error

--- dataset/management/commands/_exp_check.py
+++ dataset/management/commands/_exp_check.py
@@ -13,9 +13,11 @@ class Pattern():
         return None
 
 
-#one data file with all sample data in it
-#E-MTAB-1169
 class DP_E_GEOD_4006(Pattern):
+    """
+        one data file with all sample data in it
+        E-MTAB-1169
+    """
 
     sample_keys = ['Scan Name', 'Hybridization Name']
     data_keys = ['Scan REF', 'Hybridization REF']
@@ -25,47 +27,46 @@ class DP_E_GEOD_4006(Pattern):
     def name(self):
         return self.name
 
-    #check if sdrf and data both match this pattern
+    # check if sdrf and data both match this pattern
     def is_valid(self, sdrf, data, platform):
-        #must be single data file
+        # must be single data file
         if len(data.values()) != 1:
             return None
 
         sample_key = self.match_any(self.sample_keys, sdrf.columns)
         if sample_key is None:
             return None
-
         platform_key = self.match_any(self.platform_keys, sdrf.columns)
         if platform_key is None:
             return None
-
         f = data.values()[0]
         f.seek(0)
-        header = f.readline().strip().split('\s')
-        if self.match_any(self.data_keys, header) == None:
+        header = f.readline().strip().split('\t')
+        print header
+        print self.data_keys
+        if self.match_any(self.data_keys, header) is None:
             return None
         f.seek(0)
         header[0] = 'REPORTERS'
-        df = pandas.read_table(f, names=header, skiprows=2, \
-                               index_col=['REPORTERS'], delimiter='\s')
+        df = pandas.read_table(f, names=header, skiprows=2,
+                               index_col=['REPORTERS'], delimiter='\t')
         df = df.sort_index(axis=1).sort_index(axis=0)
         sdrf = sdrf.sort(columns=sample_key)
-        #filter by platform
+        # filter by platform
         sdrf = sdrf[sdrf[platform_key].isin([platform])]
         if len(sdrf[sample_key]) > 100:
             logging.info('more than 100 samples, ignore')
             return None
-        print sample_key
-        print list(sdrf[sample_key])
-        print list(df.columns)
         df = df.loc[:, list(sdrf[sample_key])]
         return (sdrf, df)
 
 
-#one data file with one sample data, might contains junk
-#E-GEOD-26688
-#E-MEXP-3476
 class DP_E_GEOD_26688(Pattern):
+    """
+        one data file with one sample data, might contains junk
+        E-GEOD-26688
+        E-MEXP-3476
+    """
 
     name = '26688'
     sample_keys = ['Derived Array Data File']
@@ -81,8 +82,8 @@ class DP_E_GEOD_26688(Pattern):
             return None
         f = data.values()[0]
         f.seek(0)
-        header = f.readline().split('\s')
-        if self.match_any(self.data_keys, header) == None:
+        header = f.readline().split('\t')
+        if self.match_any(self.data_keys, header) is None:
             return None
         platform_key = self.match_any(self.platform_keys, sdrf.columns)
         if platform_key is None:
@@ -98,14 +99,15 @@ class DP_E_GEOD_26688(Pattern):
                 continue
             f = data[e]
             f.seek(0)
-            df = pandas.read_table(f, names=['REPORTERS', e],\
-              skiprows=1, usecols=['REPORTERS', e], \
+            df = pandas.read_table(
+                f, names=['REPORTERS', e],
+                skiprows=1, usecols=['REPORTERS', e],
                 index_col=['REPORTERS'])
             if df_total is None:
                 df_total = df
             else:
-                df_total = pandas.merge(df_total, df, left_index=True,\
-                                            right_index=True)
+                df_total = pandas.merge(df_total, df, left_index=True,
+                                        right_index=True)
         if df_total is None:
             return None
         df_total = df_total.sort_index(axis=1).sort_index(axis=0)
@@ -122,15 +124,15 @@ class ExperimentDataParse:
         self.sdrf = er.sdrf
         self.info = er.info
         self.data = er.data_raw
-        #self.pattern = None
+        # self.pattern = None
         self.patterns = [DP_E_GEOD_4006(), DP_E_GEOD_26688()]
 
     def parse(self):
-        #load sdrf into pandas data frames
+        # load sdrf into pandas data frames
         self.sdrf.seek(0)
         self.sdrf = pandas.read_table(self.sdrf, header=0)
-        #try to load processed data into pandas data frames
-        #and record the pattern
+        # try to load processed data into pandas data frames
+        # and record the pattern
         ret = None
         for p in self.patterns:
             ret = p.is_valid(self.sdrf, self.data, self.platform)
