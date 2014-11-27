078c30a5919a031d7a3769c54bef87cf7ace7e77
Jin Xuefeng <Jin Xuefeng@localhost>
2014-11-27 10:53:58 +0800

    admin customize

--- dataset/admin.py
+++ dataset/admin.py
@@ -1,24 +1,37 @@
 from django.contrib import admin
 from dataset.models import (BiogpsDataset, BiogpsDatasetData,
                             BiogpsDatasetMatrix, BiogpsDatasetPlatform,
-                            BiogpsDatasetGeoLoaded)
+                            BiogpsDatasetFailed)
+
+
 # Register your models here.
 class BiogpsDatasetAdmin(admin.ModelAdmin):
     pass
 admin.site.register(BiogpsDataset)
 
+
 class BiogpsDatasetDataAdmin(admin.ModelAdmin):
     pass
 admin.site.register(BiogpsDatasetData)
+
 
 class BiogpsDatasetMatrixAdmin(admin.ModelAdmin):
     pass
 admin.site.register(BiogpsDatasetMatrix)
 
+
 class BiogpsDatasetPlatformAdmin(admin.ModelAdmin):
     pass
 admin.site.register(BiogpsDatasetPlatform)
 
-class BiogpsDatasetGeoLoadedAdmin(admin.ModelAdmin):
+# 
+# class BiogpsDatasetGeoLoadedAdmin(admin.ModelAdmin):
+#     pass
+# admin.site.register(BiogpsDatasetGeoLoaded)
+
+
+class BiogpsDatasetFailedAdmin(admin.ModelAdmin):
     pass
-admin.site.register(BiogpsDatasetGeoLoaded)
+admin.site.register(BiogpsDatasetFailed)
+
+
