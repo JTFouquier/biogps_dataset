6f7b27ad9b227e1f2624e3538ba0617b813629c5
Jin Xuefeng <Jin Xuefeng@localhost>
2014-12-16 10:42:59 +0800

    remove lint check warning

--- dataset/management/commands/ds_import.py
+++ dataset/management/commands/ds_import.py
@@ -1,8 +1,9 @@
-#-*-coding: utf-8 -*-                               
-from dataset import models                          
-from elasticsearch import  Elasticsearch            
+# -*-coding: utf-8 -*-
+
+from dataset import models
 from django.core.management.base import BaseCommand
-from django.conf import settings 
+from django.conf import settings
+
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
@@ -11,20 +12,18 @@ class Command(BaseCommand):
             using("default_dataset").all().delete()
 
         ds_set = models.BiogpsDataset.objects.using("default_ds").\
-        filter(id__in = settings.DEFAULT_DS_ID)
+            filter(id__in=settings.DEFAULT_DS_ID)
         print 'write new data'
-        for set_item in ds_set:
-            ds_plt = set_item.platform
-            if ds_plt.id in plt_dic :
+        platforms = []
+        for ds in ds_set:
+            plt = ds.platform
+            if plt.id in platforms:
                 pass
             else:
-                plt_dic.append(ds_plt.id)
-                ds_plt.save(using = "default_dataset")
-
-            set_item.save(using = "default_dataset")
-
-            ds_datas =  set_item.dataset_data.all()
+                platforms.append(plt.id)
+                plt.save(using="default_dataset")
+            ds.save(using="default_dataset")
+            ds_datas = ds.dataset_data.all()
             for data_item in ds_datas:
-                data_item.save(using = "default_dataset")
-            
+                data_item.save(using="default_dataset")
         print "done"
