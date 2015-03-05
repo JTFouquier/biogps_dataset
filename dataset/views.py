8cb1b11d3aca6903f49c31a40455c4bf32104160
Jin Xuefeng <Jin Xuefeng@localhost>
2015-03-05 16:30:38 +0800

    return geo_gse_id

--- dataset/views.py
+++ dataset/views.py
@@ -149,8 +149,9 @@ def dataset_list(request):
         qs = models.BiogpsDataset.objects.all().order_by('-created')
     else:
         qs = models.BiogpsDataset.objects.all().order_by('created')
-    ret = [{'id': ds.id, 'name': ds.name, 'slug': ds.slug}
-           for ds in qs[(page-1)*page_by: page*page_by]]
+    ret = [{'id': ds.id, 'name': ds.name, 'slug': ds.slug,
+            'geo_gse_id': ds.geo_gse_id} for ds in
+           qs[(page-1)*page_by: page*page_by]]
 #     ds = qs.values_list('id', 'name', 'slug',
 #                         'summary')[(page-1)*page_by: page*page_by]
     return general_json_response(detail=ret)
