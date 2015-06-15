e71feac383757c2b8f7e5771b63005e4e253c149
newgene <anewgene@yahoo.com>
2015-06-15 23:55:32 +0000

    using settings_prod.py in prod wsgi application.

--- biogps_dataset/wsgi.py
+++ biogps_dataset/wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "biogps_dataset.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "biogps_dataset.settings_prod")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
